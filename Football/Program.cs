1c2017b84f7b8e135f3c0ccea67fa22f9c8dcb51
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-13 19:41:04 -0600

    commit 1 formatting update

--- Football/Program.cs
+++ Football/Program.cs
@@ -13,7 +13,7 @@ namespace Football
             int fieldgoals = int.Parse(Console.ReadLine());
 
             // Compute the number of points in a single line of code.
-            int points = touchdowns * 7 + fieldgoals*3;
+            int points = touchdowns * 7 + fieldgoals * 3;
 
             Console.WriteLine($"The team scored {points} points.");
 
